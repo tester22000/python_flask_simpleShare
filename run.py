32d5cd74c480fd59c4d3cd2d3abdc1d7520c0fdb
tester22000 <95004302+tester22000@users.noreply.github.com>
2025-08-27 17:31:49 +0900

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app.database import  create_table_if_not_exists
 
 app = create_app()
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+    app.run(host='0.0.0.0', port=5000)
